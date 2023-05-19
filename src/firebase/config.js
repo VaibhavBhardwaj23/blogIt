5e3c5d58ac5c00b22030c60b4d9373f6580cb707
VaibhavBhardwaj23 <majesty.v2507@gmail.com>
2023-05-20 00:39:59 +0530

    final blog

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyDrOx5_hKb7aF9YVXspr4cSgd8jwkXZFpQ",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "blog-it-de8cb.firebaseapp.com",
   projectId: "blog-it-de8cb",
   storageBucket: "blog-it-de8cb.appspot.com",
   messagingSenderId: "406381857695",
-  appId: "1:406381857695:web:87d06026fba5b28b024baa",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
